518a169d91c8ce5c7eb4dc87a14b7bb8ceb32886
MatCat776 <138727313+MatCat776@users.noreply.github.com>
2025-12-15 11:45:07 -0800

    bad idea

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ print("Running main function")
 hello1.say_hi()
 hello2.say_hi2("This is a message from main")
 print("End of main function")
+
+print("I had a plan, but it was a bad plan. Now I'm just rambling...")
