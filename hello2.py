eaf6873d114b373fd19f0986b7fbf043fb8fd7a1
MatCat776 <138727313+MatCat776@users.noreply.github.com>
2025-12-15 11:34:40 -0800

    Sample files added

---
+++ hello2.py
@@ -0,0 +1,3 @@
+def say_hi2(message):
+    print("Hello from file 2")
+    print("Message is: " + message)
---
+++ main.py
@@ -0,0 +1,7 @@
+import hello1
+import hello2
+
+print("Running main function")
+hello1.say_hi()
+hello2.say_hi2("This is a message from main")
+print("End of main function")
